831e0774ad384efcedb7384d4e744255be8cbc59
jeongjin kim <valorjj@gmail.com>
2023-02-08 16:32:55 +0900

    need to dig something about docker (Dockerfile)

--- src/main/java/com/example/security_01/common/security/SecurityConfig.java
+++ src/main/java/com/example/security_01/common/security/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.*;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
